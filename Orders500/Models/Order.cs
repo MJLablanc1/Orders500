96bb389aef5ecd7136f70e6bd9977425baabe9d1
Ian Beard <ianbeard0159@gmail.com>
2022-05-24 13:21:40 -0700

    Added Data Models

---
+++ Orders500/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orders500.Models
+{
+    public class Order
+    {
+        public int storeID { get; set; }
+        public int salesPersonID { get; set; }
+        public int cdPrice { get; set; }
+        public int cdID { get; set; }
+        public DateTime date { get; set; }
+    }
+}
